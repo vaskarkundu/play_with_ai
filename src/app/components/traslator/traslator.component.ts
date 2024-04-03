7bbca118db764b40485d6169b1f593f3ab89005a
Vaskar Kundu <vaskar.kundu@nexdecade.com>
2024-04-03 13:02:53 +0600

    feat : added open ai api service

--- src/app/components/traslator/traslator.component.ts
+++ src/app/components/traslator/traslator.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { OpenaiService } from 'src/app/services/openai_api_service';
 
 @Component({
   selector: 'app-traslator',
   templateUrl: './traslator.component.html',
-  styleUrls: ['./traslator.component.css']
+  styleUrls: ['./traslator.component.css'],
 })
 export class TraslatorComponent {
+  constructor(private AiServices: OpenaiService) {}
 
+  ngOnInit() {
+    console.log('hi');
+    this.getOpenaiCompletion();
+  }
+
+  async getOpenaiCompletion() {
+    try {
+      const completion = await this.AiServices.getCompletion('a');
+      console.log('OpenAI Completion:', completion);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 }
