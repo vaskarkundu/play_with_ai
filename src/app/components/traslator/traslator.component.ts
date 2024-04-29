6469ff0d644e2d007ede059ea390e4e6e02f7718
Vaskar Kundu <vaskar.kundu@nexdecade.com>
2024-04-29 14:43:52 +0600

    feat : added refactive form fromarray

--- src/app/components/traslator/traslator.component.ts
+++ src/app/components/traslator/traslator.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OpenaiService } from 'src/app/services/openai_api_service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 interface Language {
   id: string;
   name: string;
@@ -30,19 +30,31 @@ export class TraslatorComponent {
 
   ngOnInit() {
     // this.getOpenaiCompletion();
-    this.translatorForm = this.formBuilder.group({
-      sourceLanguage: [null],
-      targetLanguage: [null],
-    });
-    // this.getTranslatorForm();
+
+    this.getTranslatorForm();
   }
 
-  // getTranslatorForm() {
-  //   this.translatorForm = this.formBuilder.group({
-  //     sourceLanguage: [null],
-  //     targetLanguage: [null],
-  //   });
-  // }
+  getTranslatorForm() {
+    this.translatorForm = this.formBuilder.group({
+      sourceLanguage: [null],
+
+      targetLanguage: this.formBuilder.array([]),
+    });
+
+    this.addTarget();
+  }
+
+  get targetLanguag() {
+    return this.translatorForm.get('targetLanguage') as FormArray;
+  }
+
+  addTarget() {
+    const targetGroup = this.formBuilder.group({
+      targetValue: [null],
+      // targetInput: [null],
+    });
+    this.targetLanguag.push(targetGroup);
+  }
 
   async getOpenaiCompletion() {
     try {
@@ -55,5 +67,8 @@ export class TraslatorComponent {
 
   onSubmit() {
     console.log('submit', this.translatorForm.value);
+  }
+  reverse() {
+    console.log('d');
   }
 }
