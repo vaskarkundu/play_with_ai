04b9beb70cf0df1f635b455f7230ceea33732baf
Vaskar Kundu <vaskar.kundu@nexdecade.com>
2024-05-30 17:09:55 +0600

    refactor : app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TraslatorComponent } from './components/traslator/traslator.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
   },
-  {
-    path: 'traslator',
-    component: TraslatorComponent,
-  },
 
-  //   {
-  //     path: 'auth',
-  //     loadChildren: () =>
-  //       import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  //   },
+  {
+    path: 'tools',
+    loadChildren: () =>
+      import('./modules/tools/tools.module').then((m) => m.ToolsModule),
+  },
 ];
 
 @NgModule({
