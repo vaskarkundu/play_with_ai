c79821ac1d359171082a4fe371bb12dbaf05ae94
Vaskar Kundu <vaskar.kundu@nexdecade.com>
2024-06-05 15:28:06 +0600

    feat : added note-card to summarization

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,9 +3,36 @@ import { CommonModule } from '@angular/common';
 import { HeadingComponent } from './heading/heading.component';
 import { NavigationButtonComponent } from './navigation-button/navigation-button.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NotecardComponent } from './notecard/notecard.component';
+
 @NgModule({
-  declarations: [HeadingComponent, NavigationButtonComponent],
-  imports: [CommonModule],
-  exports: [HeadingComponent, NavigationButtonComponent],
+  declarations: [
+    HeadingComponent,
+    NavigationButtonComponent,
+    NotecardComponent,
+  ],
+  imports: [
+    CommonModule, // for material lib
+    MatSlideToggleModule,
+    MatGridListModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
+  exports: [HeadingComponent, NavigationButtonComponent, NotecardComponent],
 })
 export class SharedModule {}
--- src/app/modules/tools/summarize/summarize.component.ts
+++ src/app/modules/tools/summarize/summarize.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-summarize',
   templateUrl: './summarize.component.html',
-  styleUrls: ['./summarize.component.css']
+  styleUrls: ['./summarize.component.css'],
 })
 export class SummarizeComponent {
+  title: string = 'AI Summarization Tools';
+  noteHeader: string = '**Notes';
 
+  notes: string[] = [
+    'Accept a text file with up to 1000 words, and identify or specify keywords from which you can summarize the content.',
+    'Accept a JSON file, focus on keywords, extract key points for summarization, and provide the result in either text or JSON format.',
+    'Accept a CSV file, focus on keywords, extract key points for summarization, and provide the result in text, JSON, or CSV format.',
+  ];
 }
