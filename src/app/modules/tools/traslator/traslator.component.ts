e6b55a6213cb4280574bc858dfb33b724e6e2a65
Vaskar Kundu <vaskarkundu18@gmail.com>
2024-06-06 17:47:58 +0600

    feat : added note-card component to traslator

--- src/app/modules/tools/traslator/traslator.component.ts
+++ src/app/modules/tools/traslator/traslator.component.ts
@@ -16,6 +16,12 @@ interface Language {
   styleUrls: ['./traslator.component.css'],
 })
 export class TraslatorComponent {
+  noteHeader: string = '**Notes';
+  notes: string[] = [
+    'Accept a text file with up to 1000 words, and identify or specify keywords from which you can summarize the content.',
+    'Accept a JSON file, focus on keywords, extract key points for summarization, and provide the result in either text or JSON format.',
+    'Accept a CSV file, focus on keywords, extract key points for summarization, and provide the result in text, JSON, or CSV format.',
+  ];
   massage: any = [
     {
       role: 'system',
